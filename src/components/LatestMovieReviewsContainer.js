39296795ce79aec4cb8725cbe7370300c2309338
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 18:59:53 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -5,5 +5,3 @@ import MovieReviews from './MovieReviews'
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
-
-// Code LatestMovieReviewsContainer Here
