11c74d2b08d27d1b96bca6de9bd048a45a7956fb
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 19:08:05 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,7 +7,10 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 class SearchableMovieReviewsContainer extends Component {
-  
+  state = {
+    searchTerm: '',
+    reviews: []
+  }
 }
 
 export default SearchableMovieReviewsContainer;
