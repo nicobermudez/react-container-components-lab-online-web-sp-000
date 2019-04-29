82a1a6abf1708ebcb35bdec36183e912f11c4c45
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 19:14:17 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,4 +1,15 @@
 import React from 'react';
 const Review = ({
-  
-})
+    headline,
+    byline,
+    link,
+    summary_short
+}) => {
+  return (
+    <div
+      key={headline}
+      className="review"
+    >
+    </div<
+  )
+}
