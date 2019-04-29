f686122155f92e3602c2bca0f151b05c91680766
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 19:19:26 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-
-import MovieReviews from './MovieReviews';
+import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
@@ -13,24 +12,22 @@ class LatestMovieReviewsContainer extends Component {
 
     this.state = {
       reviews: []
-    };
+    }
   }
 
   componentDidMount() {
     fetch(URL)
-      .then(res => res.json())
-      .then(response => this.setState({ reviews: response.results }));
+    .then(response => response.json())
+    .then(data => this.setState({reviews: data.results}))
   }
 
   render() {
     return (
       <div className="latest-movie-reviews">
-        <h2>The Latest Reviews:</h2>
+        <h2>The Latest Reviews: </h2>
         <MovieReviews reviews={this.state.reviews} />
       </div>
-    );
+    )
   }
 }
-
-
 export default LatestMovieReviewsContainer;
